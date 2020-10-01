a27a8e0695bdfefd714055c32498ef6611c5877b
shanpig <shanpigliao@gmail.com>
2020-10-01 19:54:50 +0800

    Updates

---
+++ precache-manifest.af4045b2baaacf994170b66e15a3ae5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1ee961173e78b84718c6ee3624e9db",
+    "url": "/food-searcher/index.html"
+  },
+  {
+    "revision": "727398488563b52ecc7a",
+    "url": "/food-searcher/static/css/main.252b82ad.chunk.css"
+  },
+  {
+    "revision": "10beebeab60c88c15141",
+    "url": "/food-searcher/static/js/2.6b2af478.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-searcher/static/js/2.6b2af478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "727398488563b52ecc7a",
+    "url": "/food-searcher/static/js/main.a8ce512d.chunk.js"
+  },
+  {
+    "revision": "82a6ab2aa75fbef1d837",
+    "url": "/food-searcher/static/js/runtime-main.394441d7.js"
+  },
+  {
+    "revision": "ebc5918e4650921ec536bcef8254aa2b",
+    "url": "/food-searcher/static/media/background.ebc5918e.jpg"
+  }
+]);
